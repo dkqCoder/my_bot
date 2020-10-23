52c64004d2f46a7b3e600764ea021075d608f269
shihui <shihui@beeplay123.com>
2020-10-23 15:37:05 +0800

    出票V1

--- cp_tty_service/src/main/java/com/tty/common/utils/LotteryUtil.java
+++ cp_tty_service/src/main/java/com/tty/common/utils/LotteryUtil.java
@@ -3,9 +3,7 @@ package com.tty.common.utils;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-/**
- * Created by yys on 2017/7/6.
- */
+
 public class LotteryUtil {
     public static Integer getUnitMoney(Integer playTypeId) {
         if (playTypeId == 3903 || playTypeId == 3904 || playTypeId == 3908) {
